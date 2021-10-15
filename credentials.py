157ce82cce8adcfa954d42e26d01416be59258f5
Nemat <47951122+nemathsnv@users.noreply.github.com>
2021-10-15 04:12:06 +0400

    Initial commit

---
+++ credentials.py
@@ -0,0 +1,12 @@
+twitter_app = "Nemat Hasanov's Twitter Research"
+consumer_key = 'UFKKwTAb94m1zMpnbbTg57Gsg'
+consumer_secret = '03wTb9hhF89ikZNCMegKfg6wPUzZthbwguNIt5ETKxGP5uvPWx'
+access_token = '593016576-TkLeCzNRaA1aKeNSHgnQh1ZIESoazwYReSsoMad0'
+access_token_secret = 'wxdcFGT4TazulOZUQjYUKutvHvoEjycF0Zhp8ImsnU8a3'
+
+
+# twitter_app = 'Twitter for Mingəçevir'
+# consumer_key = 'EKIEBM63xeaHuy8W6hGImebAE'
+# consumer_secret = '6oob8IE7vQsZadQk6tKASFTgWInTJQVz71efn1l2Yb47T3DvZe'
+# access_token = '593016576-yhlByx539QZBP2C29OtUAAxt9MDpRiGILX47MRNX'
+# access_token_secret = 'Fxh3JPmgcR4IiNv6cnhtY5HJuI5WVzqo2WStQANplwUI2'
